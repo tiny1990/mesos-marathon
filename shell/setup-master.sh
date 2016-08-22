4c47b319085cd2c259ae8e30e943a37373cb8419
p_mx <tiny1990@126.com>
2016-08-22 15:03:17 +0800

    Update setup-master.sh

--- shell/setup-master.sh
+++ shell/setup-master.sh
@@ -8,10 +8,10 @@ SLAVE_IP=$(cat slave)
 
 install_mesos
 uninstall_mesos_slave
-install_etcd '$MASTER_IP:2379'
+install_etcd $MASTER_IP:2379
 install_zookeeper
 
 start_mesos_master
 start_zookeeper
 
-start_mesos_marathon '$MASTER_IP:5050' '$MASTER_IP:2181'
+start_mesos_marathon $MASTER_IP:5050 $MASTER_IP:2181
