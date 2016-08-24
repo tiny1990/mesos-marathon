4b5ec6569c74b2c4063287e6212d34f7af05e68f
p_mx <tiny1990@126.com>
2016-08-24 14:47:32 +0800

    Update install-centos7.sh

--- shell/install-centos7.sh
+++ shell/install-centos7.sh
@@ -76,7 +76,8 @@ start_mesos_master()
 
 start_mesos_marathon()
 {
-	sudo marathon --master $1:5050 --hostname $1 --zk zk://$2 --http_port 8888  --enable_features external_volumes &
+	#  --enable_features external_volumes is not support?
+	sudo marathon --master $1:5050 --hostname $1 --zk zk://$2 --http_port 8888 &
 }
 
 start_mesos_slave()
